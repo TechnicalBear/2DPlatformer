29af849b8469737e91754c7d0e9dc2155ded3ca6
Bearz <TechnicalBear@gmail.com>
2015-03-08 23:00:42 +0000

    Worked on map system
    Encountering a few errors but all else seems to be in place

--- main.lua
+++ main.lua
@@ -1,7 +1,14 @@
+local AdvTiledLoader = require ("AdvTiledLoader")
+
 function love.load()
+	AdvTiledLoader.path = "maps/"
+	map = AdvTiledLoader/load("map.tmx")
+	map:setDrawRange(0, 0, map.width * map.tileWidth, map.height * map.tileHeight)
+
 end
 
 function love.draw()
+	map:draw()
 end
 
 function love.update(dt)
